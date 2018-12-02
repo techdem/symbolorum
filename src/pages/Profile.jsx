49241a756b30e99898f99e227d2f47b3311c2cf3
techdem <tudor.chiribes@gmail.com>
2018-12-02 16:22:26 +0000

    updated profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,17 +1,100 @@
 import React, { Component } from 'react';
-import { Lead } from 'bootstrap-4-react';
-import { BSpan } from 'bootstrap-4-react';
-import { Authenticator } from 'aws-amplify-react';
+import { Auth, Logger } from 'aws-amplify';
+import { Container, Form, InputGroup, Button, Alert } from 'bootstrap-4-react';
 
-export default class Home extends Component {
-  render() {
+const logger = new Logger('Profile');
+
+export default class Profile extends Component {
+  constructor(props) {
+    super(props);
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.loadProfile = this.loadProfile.bind(this);
+    this.saveProfile = this.saveProfile.bind(this);
+    this.state = { profile: {given_name: '', family_name: ''} };
+  }
+
+  componentDidMount() {
+    if (this.props.user) { this.loadProfile() }
+  }
+
+  componentDidUpdate(prevProps) {
+    if (!prevProps.user && this.props.user) {
+      this.loadProfile();
+    }
+  }
+
+  handleInputChange(name, value) {
+    const profile = Object.assign({}, this.state.profile);
+    profile[name] = value;
+    this.setState({ profile: profile });
+  }
+
+  loadProfile() {
     const { user } = this.props;
-    
+    Auth.userAttributes(user)
+      .then(data => this.loadSuccess(data))
+      .catch(err => this.handleError(err));
+  }
+
+  saveProfile() {
+    const { user } = this.props;
+    if (!user) {
+      this.handleError('No user to save to');
+      return;
+    }
+
+    Auth.updateUserAttributes(user, this.state.profile)
+      .then(data => this.saveSuccess(data))
+      .catch(err => this.handleError(err));
+  }
+
+  loadSuccess(data) {
+    logger.info('loaded user attributes', data);
+    const profile = this.translateAttributes(data);
+    this.setState({ profile: profile });
+  }
+
+  saveSuccess(data) {
+    logger.info('saved user profile', data);
+  }
+
+  handleError(error) {
+    logger.info('load / save user attributes error', error);
+    this.setState({ error: error.message || error });
+  }
+
+  translateAttributes(data) {
+    const profile = {};
+    data
+      .filter(attr => ['given_name', 'family_name'].includes(attr.Name))
+      .forEach(attr => profile[attr.Name] = attr.Value);
+    return profile;
+  }
+
+  render() {
+    const { profile, error } = this.state;
+
     return (
-      <React.Fragment>
-        { !user && <Authenticator /> }
-        { user && <Lead>You are signed in as <BSpan font="italic">{user.username}</BSpan>.</Lead> }
-      </React.Fragment>
+      <Container display="flex" flex="column" alignItems="center">
+        <InputGroup mb="3" style={{ maxWidth: '24rem' }}>
+          <InputGroup.PrependText>First name</InputGroup.PrependText>
+          <Form.Input
+            type="text"
+            defaultValue={profile.given_name}
+            onChange={event => this.handleInputChange('given_name', event.target.value)}
+          />
+        </InputGroup>
+        <InputGroup mb="3" style={{ maxWidth: '24rem' }}>
+          <InputGroup.PrependText>Last name</InputGroup.PrependText>
+          <Form.Input
+            type="text"
+            defaultValue={profile.family_name}
+            onChange={event => this.handleInputChange('family_name', event.target.value)}
+          />
+        </InputGroup>
+        <Button primary px="5" onClick={this.saveProfile}>Save</Button>
+        { error && <Alert warning>{error}</Alert> }
+      </Container>
     )
   }
 }
